32c9af3f68aab084fd1826ee50202975e71399f5
Aman Bhardwaj <ryuzaki@Amans-MacBook-Air.local>
2020-10-05 20:56:36 +0530

    init

---
+++ components/QuestionItem.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'next/link'
+import { useQuery, useMutation } from '@apollo/client'
+import { getMeQuery } from '../src/queries/auth'
+import { deleteQuestionMutation, fetchAllQuestionsQuery } from '../src/queries/questions'
+import Preloader from './Preloader'
+
+const QuestionItem = ({ question: { title, tags, description, _id, user: { name }, user } }) => {
+
+    const { loading, data } = useQuery(getMeQuery);
+
+    const [deleteQuestion, mutationResponse] = useMutation(deleteQuestionMutation, {
+        variables: {
+            id: _id
+        },
+        refetchQueries: [
+            {
+                query: fetchAllQuestionsQuery,
+            }
+        ]
+    });
+
+    if (loading || mutationResponse.loading) {
+        return <Preloader />
+    }
+
+    const onDelete = e => {
+        deleteQuestion().then(() => {
+            M.toast({ html: 'Question Deleted!' })
+        }).catch(err => {
+            M.toast({ html: err });
+        });
+    }
+
+    return (
+        <div className='col s12 m6 l6'>
+            <div className="card grey darken-3">
+                <div className="card-content">
+                    <span className="card-title">
+                        {title}
+                    </span>
+                    <p>
+                        ~ <span className="grey-text">{name}</span>
+                    </p>
+                </div>
+                <div className="card-action">
+                    <Link href='/question/[id]' as={`/question/${_id}`}>
+                        <a>Explore</a>
+                    </Link>
+                    {data && data.getMe._id === user._id ? <a href="#!" className='secondary-content' onClick={onDelete}>
+                        Delete
+                    </a> : null}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+QuestionItem.propTypes = {
+    question: PropTypes.object.isRequired,
+}
+
+export default QuestionItem
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+
+
+const index = () => {
+  return (
+    <div className='container'>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default index
