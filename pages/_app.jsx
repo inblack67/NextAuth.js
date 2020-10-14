c364c574af15f132940cacd878ac3b5f0d9be371
Aman Bhardwaj <devproddeploy@gmail.com>
2020-10-14 09:01:18 +0530

    localhost

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,13 +3,13 @@ import '../styles/globals.css';
 import { Provider } from 'next-auth/client';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Provider session={pageProps.session}>
-      <Layout>
-        <Component {...pageProps} />;
-      </Layout>
-    </Provider>
-  );
+	return (
+		<Provider session={pageProps.session}>
+			<Layout>
+				<Component {...pageProps} />;
+			</Layout>
+		</Provider>
+	);
 }
 
 export default MyApp;
